53c151cc698608535da3988cedee4ae00702ba63
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2016-09-19 19:17:11 +0900

    add month_constrain/active_recrd_base

---
+++ lib/month/constrain/active_record_base.rb
@@ -0,0 +1,22 @@
+require 'month/constrain/active_record_base/initializer'
+
+module Month::Constrain
+  module ActiveRecordBase
+    def acts_as_month_constrain(*columns)
+      Initializer.setter(self, columns)
+      Initializer.scope(self, columns)
+    end
+
+    def month_constrain(value)
+      case value
+      when Date, Time, ActiveSupport::TimeWithZone
+        Date.new(value.year, value.month, 1)
+      when /\A(\d{4})-(\d{1,2})/
+        Date.new(Regexp.last_match(1).to_i, Regexp.last_match(2).to_i, 1)
+      end
+    rescue ArgumentError => ex
+      logger.warn("#{ex.message} (#{ex.class})")
+      nil
+    end
+  end
+end
