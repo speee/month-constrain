662f344f911ea06028a5fbc8be582c1e22736d12
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2016-09-21 20:15:15 +0900

    add month_constrain logic

---
+++ lib/month_constrain/active_record_base/initializer.rb
@@ -0,0 +1,31 @@
+module MonthConstrain::ActiveRecordBase
+  module Initializer
+    def self.setter(target_class, columns)
+      target_class.class_eval do
+        columns.each do |column|
+          define_method "#{column}=" do |val|
+            super(self.class.month_constrain(val))
+          end
+        end
+      end
+    end
+
+    def self.scope(target_class, columns)
+      target_class.class_eval do
+        columns.each do |column|
+          scope "#{column}_in".to_sym, -> (from, to) do
+            relation = self
+            relation = relation.where("#{column} >= ?", month_constrain(from)) if from
+            relation = relation.where("#{column} <= ?", month_constrain(to)) if to
+            relation
+          end
+
+          scope "#{column}".to_sym, -> (val) do
+            where(column => month_constrain(val))
+          end
+        end
+      end
+    end
+  end
+end
+
