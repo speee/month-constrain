d235899a9934ff2f837767388d38e0e8329b6e35
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2016-09-21 20:15:41 +0900

    add spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'month_constrain'
+require 'bundler/setup'
+Bundler.require
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
